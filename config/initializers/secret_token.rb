ca53b0cdfd590b138254490f144630333035faa2
Dmtiriy Budnik <dmitriy.budnik+github@gmail.com>
2013-05-13 20:02:40 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sikleets::Application.config.secret_token = '4f31ff85e478066f6e15b5f99f3468ee50905e23986ea500dd2873a8cd18be997187b603c85492233d927ea9a1631076b63052f6db459e8301a5afd13afbd107'
