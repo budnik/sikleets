66f6a6a7e3b7870fb9feabcda130ca5224bf8039
Dmtiriy Budnik <dmitriy.budnik+github@gmail.com>
2013-05-13 21:35:21 +0300

    Revert "twitter initializer"
    This reverts commit ecbaa5615c4fa40b3c6e2035183f23570a68ac34.

--- config/initializers/twitter.rb
+++
@@ -1,6 +0,0 @@
-Twitter.configure do |config|
-  config.consumer_key = ENV['TW_C_KEY']
-  config.consumer_secret = ENV['TW_C_SEC']
-  config.oauth_token = ENV['TW_A_TKN']
-  config.oauth_token_secret = ENV['TW_A_TSEC']
-end
